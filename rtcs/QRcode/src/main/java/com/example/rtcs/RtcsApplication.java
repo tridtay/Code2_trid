35fa33c3b632785d5d8f7b6055168c4bc3d4b185
tridsanu taychathunyachot <tridsanu.tay@gmail.com>
2022-03-20 06:43:30 +0700

    add QRcode service

---
+++ rtcs/QRcode/src/main/java/com/example/rtcs/RtcsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rtcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtcsApplication.class, args);
+	}
+
+}
