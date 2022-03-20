2ae72f615a66719a7e82cf438ef18188f4e481c8
tridsanu taychathunyachot <tridsanu.tay@gmail.com>
2022-03-20 07:55:50 +0700

    add new service RTCSbalance

---
+++ rtcs/RTCSbalance/src/main/java/com/example/RTCSbalance/RtcSbalanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RTCSbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtcSbalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtcSbalanceApplication.class, args);
+	}
+
+}
